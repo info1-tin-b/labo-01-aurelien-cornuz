4b98d493fc1223f079113cfbc40da1af59c3cfa4
aurelien.cornuz <aurelien.cornuz@hei-vd.ch>
2025-09-26 18:15:53 +0200

    mise a jour du fichier gcd.c

--- gcd.c
+++ gcd.c
@@ -31,3 +31,32 @@ int main(int argc, char* argv[]) {
 
     return 0
 }
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+int gcd(int a, int b) {
+  int r;
+  while (b != 0) {
+       r = a % b;
+       a = b;
+       b = r;
+  }
+  return a;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc <= 2) {
+        return -1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+
+    int result = gcd(a, b);
+
+    printf("%d\n", result);
+    
+    return 0;
+}
+ 
+
