45724b8d6b5bd7196f3b3f3b2d1566a8dd8fd27a
aurelien-cornuz <aurelien.cornuz@heig-vd.ch>
2025-09-26 18:19:47 +0200

    New gcd.c

--- gcd.c
+++ gcd.c
@@ -6,33 +6,6 @@
  */
 #include <stdlib.h>
 #include <string.h>
-
-int gcd(int a, int b) {
-  int k;
-  while (b != 0) {
-    r = b;
-    b = b % a;
-    a = r;
-  }
-  return a;
-}
-
-int main(int argc, char* argv[]) {
-    if (argc <= 2) {
-        return -1;
-    }
-
-    int a = atoi(argv[1]);
-    int b = atoi(argv[2]);
-
-    int result = gcd(a, b);
-
-    printf("%d\n", result + 5);
-
-    return 0
-}
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 int gcd(int a, int b) {
   int r;
